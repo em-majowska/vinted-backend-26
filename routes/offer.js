6df20bd935a22dd0fc90325b4751df4f2e5ea1ae
Ewa Majowska <em.majowska@gmail.com>
2026-02-19 18:03:24 +0400

    Add route to filter offers and find offer by id

--- routes/offer.js
+++ routes/offer.js
@@ -5,16 +5,18 @@ const cloudinary = require('cloudinary').v2;
 
 const isAuthenticated = require('../middlewares/isAuthenticated');
 const convertToBase64 = require('../utils/convertToBase64');
-// const uploadPictures = require('../middlewares/uploadPictures');
+const uploadPictures = require('../services/uploadPictures');
 const Offer = require('../models/Offer');
 const { default: mongoose } = require('mongoose');
 
+// Create an offer
 router.post(
   '/offer/publish',
   isAuthenticated,
   fileUpload(),
-  // uploadPictures,
   async (req, res) => {
+    const pictures = await uploadPictures(req.files);
+
     try {
       const newOffer = new Offer({
         product_name: req.body.name,
@@ -27,28 +29,14 @@ router.post(
           { condition: req.body.condition },
           { city: req.body.city },
         ],
-        // pour l'instant j'utilise un objet pour que c'est bien de type Object
-        product_image: {},
+        product_image: pictures.length > 1 ? pictures : pictures[0] || {},
 
-        // Postman montre "owner": "6995e904efd98c0d936a5477"
         owner: req.user._id,
       });
 
-      // no need since new Document already creates en Id
-      // await newOffer.save();
-
       //Upload pictures with ID path
-      if (req.files !== null) {
-        const picture = await cloudinary.uploader.upload(
-          convertToBase64(req.files.pictures),
-          { folder: `vinted/offers/${newOffer._id}` },
-        );
-
-        newOffer.product_image = picture;
-      }
 
       await newOffer.save();
-
       await newOffer.populate('owner', 'email account');
 
       res.status(201).json({
@@ -57,12 +45,13 @@ router.post(
       });
     } catch (error) {
       res
-        .status(error.status || 400)
+        .status(error.status || 500)
         .json({ message: error.message || 'Internal server error' });
     }
   },
 );
 
+// Modify offer
 router.put(
   '/offer/modify/:id',
   isAuthenticated,
@@ -100,17 +89,72 @@ router.put(
         await offer.save();
       }
 
-      console.log('2 => ', offer);
-
       res
         .status(200)
         .json({ message: 'Offer successfully modified', data: offer });
     } catch (error) {
       res
-        .status(error.status || 400)
+        .status(error.status || 500)
         .json({ message: error.message || 'Internal server error' });
     }
   },
 );
+
+// Search offers
+
+router.get('/offers', async (req, res) => {
+  try {
+    const filters = {};
+
+    if (req.query.title) filters.title = new RegExp(`${req.query.title}`, 'i');
+    if (req.query.priceMin) filters.priceMin = req.query.priceMin;
+    if (req.query.priceMax) filters.priceMax = req.query.priceMax;
+
+    // 'price-asc' , 'price-desc' ??
+    filters.sort =
+      req.query.sort === 'price-desc' || req.query.sort === 'price-asc'
+        ? req.query.sort
+        : 'price-desc';
+    filters.page = 5 * (req.query.page - 1) || 0;
+
+    const offers = await Offer.find({
+      product_name: filters.title ?? /.*/,
+      product_price: {
+        $lte: filters.priceMax ?? Infinity,
+        $gte: filters.priceMin ?? 0,
+      },
+    })
+      .sort({ product_price: filters.sort.replace('price-', '') })
+      .select('product_name product_price')
+      .limit(5)
+      .skip(filters.page);
+
+    res.status(200).json(offers);
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json({ message: error.message || 'Internal server error' });
+  }
+});
+
+// Search offer by id
+router.get('/offers/:id', async (req, res) => {
+  try {
+    if (!mongoose.isValidObjectId(req.params.id))
+      throw res.status(400).json({ message: 'Invalid id' });
+
+    const offer = await Offer.findById(req.params.id).populate(
+      'owner',
+      'account',
+    );
+    if (!offer) throw res.status(404).json({ message: 'Item not found' });
+
+    res.status(200).json(offer);
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .json({ message: error.message || 'Internal server error' });
+  }
+});
 
 module.exports = router;
